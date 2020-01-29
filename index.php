ce51fd15561c89e0b563cca6f0a08a98a7b33ead
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2020-01-29 10:23:17 +0200

    Add styles

--- index.php
+++ index.php
@@ -5,12 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Movie List</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="content">
         <h1>Favorite Movies</h1>
 
-        <ul>
+        <ul class="movie-list">
             <li>The Matrix</li>
             <li>The Case for Christ</li>
             <li>Man of Steel</li>
